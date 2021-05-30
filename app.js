e2932377368227cd0a1a713455d35d2b133c276b
jangyunhwan <jangyunhwan@gmail.com>
2021-05-30 20:17:49 +0900

    update websocket

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ var logger = require('morgan');
 var usersRouter = require('./routes/users');
 var menuRouter = require('./routes/menus');
 var musicRouter = require('./routes/musicRouter');
+var webSocket = require('./routes/WebSocket');
 
 var app = express();
+const server = app.listen(3002, () => {
+    console.log('3002', '번 포트에서 대기중');
+});
+
+webSocket(server);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,5 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(`/users`, usersRouter);
 app.use(`/menus`, menuRouter);
 app.use(`/musics`, musicRouter);
+// app.use(`/echo`, echoServer);
 
 module.exports = app;
