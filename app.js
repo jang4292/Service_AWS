19d2f96b9b5334deab585419359b4d0286b4c36b
jangyunhwan <jangyunhwan@gmail.com>
2021-12-31 15:09:57 +0900

    Revert "update websocket"
    This reverts commit e2932377368227cd0a1a713455d35d2b133c276b.

--- app.js
+++ app.js
@@ -6,14 +6,8 @@ var logger = require('morgan');
 var usersRouter = require('./routes/users');
 var menuRouter = require('./routes/menus');
 var musicRouter = require('./routes/musicRouter');
-var webSocket = require('./routes/WebSocket');
 
 var app = express();
-const server = app.listen(3002, () => {
-    console.log('3002', '번 포트에서 대기중');
-});
-
-webSocket(server);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,6 +19,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(`/users`, usersRouter);
 app.use(`/menus`, menuRouter);
 app.use(`/musics`, musicRouter);
-// app.use(`/echo`, echoServer);
 
 module.exports = app;
