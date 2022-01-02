67e5b30912d39b6c6b4edd4f3473f6ee52557a06
jangyunhwan <jangyunhwan@gmail.com>
2022-01-02 15:55:00 +0900

    Update menus query to check type

--- routes/menus.js
+++ routes/menus.js
@@ -8,12 +8,36 @@ const db = require(path.join(__dirname, `../public/javascripts/DatabaseManager`)
 
 /* GET users listing. */
 router.get('/', cors(), (req, res, next) => {
-    const query = `SELECT * FROM service.menus`;
+    const type = req.query ? req.query.type : null;
+    if (!type) {
+        res.status(405);
+        res.json({
+            result: -1,
+            error: "menus type is null"
+        });
+        return;
+    }
+
+    const query = `SELECT * FROM service.menus WHERE ` + type.toString() + ' = 1';
     db.query(query, (err, results, fields) => {
-        if (err) console.log(err);
-        console.log(results);
+        if (err) {
+            console.log(err);
+            res.status(405);
+            res.json({
+                result: -2,
+                error: "sql error",
+                data: {
+                    errorNo: err.errno,
+                    message: err.sqlMessage
+                }
+            });
+            return;
+        }
         res.status(200);
-        res.json(results);
+        res.json({
+            result: 1,
+            data: results
+        });
     });
 });
 
